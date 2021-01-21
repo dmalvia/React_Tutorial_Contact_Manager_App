476b8affbbe3db1c5cb6c11a960d5ac3781ddba1
Dipesh Malvia <cs.dipeshmalvia@gmail.com>
2021-01-21 13:54:20 +0800

    React_Fundamentals_tutorial

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { uuid } from "uuidv4";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import user from "../images/user.png";
+
+const ContactCard = (props) => {
+  const { id, name, email } = props.contact;
+  return (
+    <div className="item">
+      <img className="ui avatar image" src={user} alt="user" />
+      <div className="content">
+        <div className="header">{name}</div>
+        <div>{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px" }}
+        onClick={() => props.clickHander(id)}
+      ></i>
+    </div>
+  );
+};
+
+export default ContactCard;
